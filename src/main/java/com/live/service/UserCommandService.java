51f81c68ab1d826c773b5e16d78e453d86e30733
soowampy <zerooil0001@gmail.com>
2026-02-16 02:12:48 +0900

    트랜잭션 및 동시성 방지 try catch문 추가

--- src/main/java/com/live/service/UserCommandService.java
+++ src/main/java/com/live/service/UserCommandService.java
@@ -10,7 +10,9 @@ import com.live.service.dto.UserCreatedDto;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import static com.live.common.exception.BusinessErrorMessage.EMAIL_DUPLICATED;
 
@@ -19,15 +21,22 @@ import static com.live.common.exception.BusinessErrorMessage.EMAIL_DUPLICATED;
 public class UserCommandService {
     private final UserRepository userRepository;
 
+    @Transactional
     public UserCreatedDto signUp(UserCreateDto dto) {
         validateDuplicateEmail(dto);
         User user = createUser(dto);
-        User saved = userRepository.save(user);
-        return new UserCreatedDto(
-                saved.getId(),
-                saved.getEmail().getEmail(),
-                saved.getName().getName(),
-                saved.getCreatedAt());
+        try {
+            User saved = userRepository.save(user);
+            return new UserCreatedDto(
+                    saved.getId(),
+                    saved.getEmail().getEmail(),
+                    saved.getName().getName(),
+                    saved.getCreatedAt());
+        } catch (DataIntegrityViolationException e) {
+            // 동시성 방지
+            throw new DuplicateEmailException(EMAIL_DUPLICATED.getMessage());
+        }
+
     }
 
     private User createUser(UserCreateDto dto) {
